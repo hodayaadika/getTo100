4136d514842a37fac6a1f14479411c3aaa99ee24
rivka <rivkamiller@hilma.tech>
2024-11-03 13:05:15 +0200

    added pages move

---
+++ src/enterPlayers.jsx
@@ -0,0 +1,8 @@
+export default function (props) {
+  return (
+    <>
+      <h1>You've entered enter Players page</h1>
+      <button onClick={props.changePage}>Start game</button>
+    </>
+  );
+}
