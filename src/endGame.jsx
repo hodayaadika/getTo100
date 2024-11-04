48774fa23106a4171c5f0cb3538fb74e073b1c89
rivka <rivkamiller@hilma.tech>
2024-11-04 15:51:33 +0200

    add text to end game

--- src/endGame.jsx
+++ src/endGame.jsx
@@ -4,15 +4,15 @@ export default function (props) {
   const players = props.players;
   return (
     <>
-      {players.map((player, index) => (
+      {/* {players.map((player, index) => (
         <PlayerBlock player={player} setPlayers onClick={() => removePlayer} />
-      ))}
-      <h1>You've entered end game page</h1>
+      ))} */}
+      <h1>hurray</h1>
+      <h1>You've Finished the game !!!</h1>
+      <h1>...</h1>
+      <p>Now what are you going to do with your life?</p>
+      <p>BTW, we have a food fairy</p>
       <button onClick={props.changePage}>Back to enter Players page</button>
     </>
   );
-}
-
-function removePlayer() {
-  return alert("Here we need to remove this player");
 }
