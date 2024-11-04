816931f956b6312c14e954f62aad72bc3f08659c
rivka <rivkamiller@hilma.tech>
2024-11-04 10:05:54 +0200

    removing when winning works with a delay of 1 move

--- components/PlayerBlock.jsx
+++ components/PlayerBlock.jsx
@@ -4,6 +4,7 @@ export default function PlayerBlock(props) {
   const player = props.player;
   const [currNumber, setCurrnumber] = useState(Math.floor(Math.random() * 99));
   const [steps, setSteps] = useState(0);
+  const [succeeded, setSucceeded] = useState(false);
 
   function addOne() {
     setCurrnumber(currNumber + 1);
@@ -21,57 +22,62 @@ export default function PlayerBlock(props) {
   }
 
   function split() {
-    setCurrnumber(currNumber / 2);
+    setCurrnumber(Math.floor(currNumber / 2));
     setSteps(steps + 1);
   }
-  return (
-    <div>
-      <h1>Player</h1>
-      <h3>Your Number: {currNumber}</h3>
-      <h3>Name: {player.name}</h3>
-      <h3>Steps: {steps}</h3>
-      <h3>
-        <button
-          className="action"
-          onClick={() => {
-            addOne();
-          }}
-        >
-          +1
-        </button>
-        <button
-          className="action"
-          onClick={() => {
-            removeOne();
-          }}
-        >
-          -1
-        </button>
-        <button
-          className="action"
-          onClick={() => {
-            dubble();
-          }}
-        >
-          *2
-        </button>
-        <button
-          className="action"
-          onClick={() => {
-            split();
-          }}
-        >
-          /2
-        </button>
-      </h3>
-    </div>
-  );
+
+  function checkSucceeded() {
+    if (currNumber >= 100) {
+      setSucceeded(true);
+    }
+  }
+
+  if (!succeeded) {
+    return (
+      <div>
+        <h1>Player</h1>
+        <h3>Your Number: {currNumber}</h3>
+        <h3>Name: {player.name}</h3>
+        <h3>Steps: {steps}</h3>
+        <h3>
+          <button
+            className="action"
+            onClick={() => {
+              addOne();
+              checkSucceeded();
+            }}
+          >
+            +1
+          </button>
+          <button
+            className="action"
+            onClick={() => {
+              removeOne();
+              checkSucceeded();
+            }}
+          >
+            -1
+          </button>
+          <button
+            className="action"
+            onClick={() => {
+              dubble();
+              checkSucceeded();
+            }}
+          >
+            *2
+          </button>
+          <button
+            className="action"
+            onClick={() => {
+              split();
+              checkSucceeded();
+            }}
+          >
+            /2
+          </button>
+        </h3>
+      </div>
+    );
+  }
 }
-
-// function getPlayerCurrNumber() {
-//   let playersList = localStorage.getItem("inGampePlaters");
-//   let playerCurrNumber = playersList[].currNumber
-//   return playerCurrNumber;
-
-// }
-function sendPlayerCurrNumber() {}
