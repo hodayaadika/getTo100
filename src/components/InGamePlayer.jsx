63edf823c9d7bd52d4a6f538b51402699df34ba5
hodaya <hodayaadika@hilma.tech>
2024-11-04 11:47:13 +0200

    local storage

---
+++ src/components/InGamePlayer.jsx
@@ -0,0 +1,5 @@
+import {useState} from "react";
+export default function InGamePlayers(props) {
+ 
+  return <ul>{savedUsersPlayers.map(props.name)}</ul>;
+}
--- src/components/inGamePlayer.jsx
+++
@@ -1,5 +0,0 @@
-export default function inGamePlayers(props){
-    
-    return<></>
-    
-}
